c571f1adf09ff53dbbc4fd4dc0fa4108ca7294a6
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-09-19 12:33:50 +0200

    Add build actions & add some information about the authors.

--- src/mui/InformationBackdrop.tsx
+++ src/mui/InformationBackdrop.tsx
@@ -8,7 +8,7 @@ import {
 } from "@mui/material";
 import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
-const TextualContent = "Information text should be added here.";
+const TextualContent = "This is the collaboration space for the replication package of the following paper: Marco Konersmann, Angelika Kaplan, Thomas Kühn, Robert Heinrich, Anne Koziolek, Ralf Reussner andJan Jürjens, Mahmood al Door, Nicolas Boltz, Marco Ehl, Dominik Fuch, Katharina Großer, Sebastian Hahner, Jan Keim, Matthias Lohr, Timur Sağlam, Sophie Schulz, and Jan-Philipp Töberg. Evaluation Methods and Replicability of Software Architecture Research Objects. In Proceedings of the 19th IEEE International Conference on Software Architecture (ICSA 2022), Hawaii, USA (virtual). IEEE, May 2022. Accepted for publication. The replication package is archived at https://zenodo.org/record/6044059";
 
 /**
  * This Backdrop appears after clicking on the info button in app bar with additional information about the webpage.
@@ -46,7 +46,7 @@ function InformationAlert() {
     <div>
       <Alert severity="info" color="info">
         {TextualContent}
-        <CustomLink text={"link"} url={"https://www.google.com"} />
+        <CustomLink text={"Repository"} url={"https://gitlab.com/SoftwareArchitectureResearch/StateOfPractice"} />
       </Alert>
     </div>
   );
