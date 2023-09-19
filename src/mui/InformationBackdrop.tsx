52177d8ca042efb0e4970d3fffae97b46b29f90d
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-09-19 12:59:04 +0200

    Add custom information about publication.

--- src/mui/InformationBackdrop.tsx
+++ src/mui/InformationBackdrop.tsx
@@ -8,7 +8,6 @@ import {
 } from "@mui/material";
 import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
-const TextualContent = "This is the collaboration space for the replication package of the following paper: Marco Konersmann, Angelika Kaplan, Thomas Kühn, Robert Heinrich, Anne Koziolek, Ralf Reussner andJan Jürjens, Mahmood al Door, Nicolas Boltz, Marco Ehl, Dominik Fuch, Katharina Großer, Sebastian Hahner, Jan Keim, Matthias Lohr, Timur Sağlam, Sophie Schulz, and Jan-Philipp Töberg. Evaluation Methods and Replicability of Software Architecture Research Objects. In Proceedings of the 19th IEEE International Conference on Software Architecture (ICSA 2022), Hawaii, USA (virtual). IEEE, May 2022. Accepted for publication. The replication package is archived at https://zenodo.org/record/6044059";
 
 /**
  * This Backdrop appears after clicking on the info button in app bar with additional information about the webpage.
@@ -45,22 +44,11 @@ function InformationAlert() {
   return (
     <div>
       <Alert severity="info" color="info">
-        {TextualContent}
-        <CustomLink text={"Repository"} url={"https://gitlab.com/SoftwareArchitectureResearch/StateOfPractice"} />
+        <p>This is the collaboration space for the replication package of the following paper: Marco Konersmann, Angelika Kaplan, Thomas Kühn, Robert Heinrich, Anne Koziolek, Ralf Reussner and Jan Jürjens, Mahmood al Door, Nicolas Boltz, Marco Ehl, Dominik Fuchß, Katharina Großer, Sebastian Hahner, Jan Keim, Matthias Lohr, Timur Sağlam, Sophie Schulz, and Jan-Philipp Töberg. Evaluation Methods and Replicability of Software Architecture Research Objects. In Proceedings of the 19th IEEE International Conference on Software Architecture (ICSA 2022), Hawaii, USA (virtual). IEEE, May 2022. Accepted for publication.<br />
+        The replication package is archived at <a href="https://zenodo.org/record/6044059">Zenodo</a>.
+        Also checkout our <a href="https://gitlab.com/SoftwareArchitectureResearch/StateOfPractice">Repository</a>.</p>
       </Alert>
     </div>
   );
 }
 
-function CustomLink(props: LinkProps) {
-  return (
-    <Link fontFamily={"inherit"} fontSize={"inherit"} href={props.url}>
-      {props.text}
-    </Link>
-  );
-}
-
-interface LinkProps {
-  text: string;
-  url: any;
-}
